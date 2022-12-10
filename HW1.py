d07b791c60f99096ced8fb3929d3325b1b23f113
WhiteBoyRicky <119951754+WhiteBoyRicky@users.noreply.github.com>
2022-12-10 16:39:35 +0300

    Homework1

---
+++ HW1.py
@@ -0,0 +1,6 @@
+name = input('Введите имя ')
+print('Привет', name)
+age = int(input('Введите возраст '))
+print(30-age)
+ost=30-age
+print('Тебе',ost,'до 30')
---
+++ legenda.py
@@ -0,0 +1,6 @@
+a=5
+print(id(a))
+a=a+1
+print(id(a))
+a=a-1
+print(id(a))
