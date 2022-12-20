bba55b05a0c2ef793a4e48c5715663f57c788e78
WhiteBoyRicky <119951754+WhiteBoyRicky@users.noreply.github.com>
2022-12-20 18:18:26 +0300

    1

---
+++ sqrNumbers/main.py
@@ -0,0 +1,11 @@
+number=int(input('Введите число: '))
+result=1
+while result**2 <= number:
+    print (result**2)
+    result=result+1
+
+else:
+    print('Вы ввели слишком большое число')
+
+#сюда дописали вредоносный код
+#программирование на c++ отстой
