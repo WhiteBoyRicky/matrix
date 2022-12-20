58f647561f2d75412c4b57e095ab5fc23ac909ba
WhiteBoyRicky <119951754+WhiteBoyRicky@users.noreply.github.com>
2022-12-20 17:17:22 +0300

    cycles

---
+++ cycles.py
@@ -0,0 +1,8 @@
+number=12
+while number>=0:
+    print (number)
+    number=number-1
+
+print('Старт!')
+
+
---
+++ weather.py
@@ -0,0 +1,12 @@
+temp = input('Введите температуру ')
+forTemperature = "| Температура "
+print ("+" + "-"*58 + "+")
+forLen=(forTemperature + temp+ ' градусов Цельсия')
+forSpace=" "*(60 - len(forLen)-1)+ "|"
+print(forLen + forSpace)
+def print
+vlaga = input('Введите влажность ')
+forVlaga = "| Влажность "
+
+def 
+
